e20a99ca91e5df11167c500fd89e6d6c982fae6e
Nikolay Stepin <nstepin@iiko.ru>
2023-12-12 23:33:53 +0800

    init folder structure

---
+++ src/app/pages/active-stage/active-stage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-stage',
+  templateUrl: './active-stage.component.html',
+  styleUrls: ['./active-stage.component.scss']
+})
+export class ActiveStageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/active-stage/active-stage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActiveStageComponent } from './active-stage.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ActiveStageComponent]
+})
+export class ActiveStageModule { }
